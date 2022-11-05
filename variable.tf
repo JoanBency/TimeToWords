eabc40727933df2f0e3365bdbf7b63c3383a55af
Joan Bency <joanbency@yahoo.in>
2022-11-05 14:21:09 +0000

    Added jenkins for CI/CD

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+    default = "ASIATUYJP7SUGWL4KOLL"
+    }
+variable "secret_key" {
+    default = "TtFPsQRaP9hsGJ3gAuWei1VygBUD1lY3RqRBe798"
+    }
+variable "region" {
+    default = "eu-west-1"
+    }
